1928e1c71fcffb35b1ae7c48c891e77d6d43a42d
Adina Pintea <adina.pintea@isa.utm.md>
2022-11-15 15:16:30 +0200

    Initial commit

---
+++ TrelloCloneTest/UnitTest1.cs
@@ -0,0 +1,21 @@
+namespace TrelloCloneTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+        }
+    }
+}
