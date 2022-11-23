b1803ef99d3604cd36afdf1ec7d701be4800426d
Adina Pintea <adina.pintea@isa.utm.md>
2022-11-23 14:34:18 +0200

    Adding tests

---
+++ TrelloClone/Services/TestService.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TrelloClone.Models;
+
+namespace TrelloClone.Services
+{
+    public class TestService
+    {
+          public List<Card> Cards = new List<Card>();
+
+          public bool AddCard(Card card)
+          {
+               if (card != null)
+               {
+                    Cards.Add(card);
+               }
+               return card != null;
+          }
+          public bool DeleteCart(int id)
+          {
+               var deletableItem = Cards.Where(c => c.Id == id).First();
+               if (deletableItem != null)
+               {
+                    Cards.Remove(deletableItem);
+               }
+               return deletableItem != null;
+          }
+          public Card GetCartById(int id)
+          {
+               var result = Cards.Find(x => x.Id == id);
+               if (result != null)
+               {
+                    return result;
+               }
+               return null;
+          }
+    }
+}
--- TrelloCloneTest/UnitTest1.cs
+++ TrelloCloneTest/UnitTest1.cs
@@ -1,21 +1,54 @@
+using TrelloClone.Services;
+
 namespace TrelloCloneTest
 {
-    [TestClass]
-    public class UnitTest1
-    {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
+     [TestClass]
+     public class UnitTest1
+     {
+          private readonly TestService _testService = new();
 
-        [TestMethod]
-        public void TestMethod2()
-        {
-        }
+          [TestMethod]
+          public void AddNotNullCard_ShouldReturnTrue()
+          {
+               var result = _testService.AddCard(new TrelloClone.Models.Card()
+               {
+                    Id = 5,
+                    Column = new TrelloClone.Models.Column() { },
+                    Contents = "123",
+                    ColumnId = 4
 
-        [TestMethod]
-        public void TestMethod3()
-        {
-        }
-    }
+               });
+               Assert.IsTrue(result);
+          }
+
+          [TestMethod]
+          public void GetById_ValidItem_ShouldBeNotNull()
+          {
+               _testService.AddCard(new TrelloClone.Models.Card()
+               {
+                    Id = 6,
+                    Column = new TrelloClone.Models.Column() { },
+                    Contents = "123",
+                    ColumnId = 4
+
+               });
+               var result = _testService.GetCartById(6);
+               Assert.IsNotNull(result);
+          }
+
+          [TestMethod]
+          public void DeleteAnItem_ShouldReturn_True()
+          {
+               _testService.AddCard(new TrelloClone.Models.Card()
+               {
+                    Id = 7,
+                    Column = new TrelloClone.Models.Column() { },
+                    Contents = "123",
+                    ColumnId = 4
+
+               });
+               var result = _testService.DeleteCart(7);
+               Assert.IsTrue(result);
+          }
+     }
 }
